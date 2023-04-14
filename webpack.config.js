83836cc73d2e69b2c7afedee2d1c52498dd013f5
HalsooSohn <noizyufo@gmail.com>
2023-04-14 11:58:20 +0900

    fix: webpack config - babel/reset-react's runtime arg should be automatic

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                     "options": {
                         "presets": [
                             "@babel/preset-env",
-                            "@babel/preset-react",
+                            ["@babel/preset-react", {"runtime": "automatic"}]
                         ],
                         "plugins": [
                             "@babel/plugin-transform-runtime"
